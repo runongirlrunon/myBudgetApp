eea0691987839e87d2aab6f5e7696f1156099847
runongirlrunon <runongirlrunon@gmail.com>
2016-04-21 13:46:51 -0700

    Initial Commit

---
+++ myBudgetApp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  myBudgetApp
+//
+//  Created by lindsey anne on 4/21/16.
+//  Copyright © 2016 Lindsey Hogg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
